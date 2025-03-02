854dc60350831641e72bf342f3d5ca5905e62a46
armaghan-906 <76613960+armaghan-906@users.noreply.github.com>
2025-03-02 23:55:48 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allows external access
+    port: 5173, // Ensure this matches your NGINX proxy_pass
+    strictPort: true, 
+    open: false,
+    cors: true,
+    allowedHosts: ['mygodai.app', 'www.mygodai.app'], // Allow your domain
+  }
 })
